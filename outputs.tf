757b4a12a3349b8d8979e959fb2ae9b28375142c
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-15 11:54:47 +0200

    style: terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "firewall_policy_id" {
   description = "The ID of the firewall policy."
-  value = azurerm_firewall_policy.this.id
+  value       = azurerm_firewall_policy.this.id
 }
 
 output "firewall_policy_name" {
   description = "The name of the firewall policy."
-  value = azurerm_firewall_policy.this.name
+  value       = azurerm_firewall_policy.this.name
 }
